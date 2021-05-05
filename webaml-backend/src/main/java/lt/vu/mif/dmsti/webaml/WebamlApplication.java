6926a4fe8004911c4fdf7f10f6fd9921fe27f9f1
vaidasj <v.jusevicius@gmail.com>
2021-05-05 18:49:44 +0300

    Initial commit

---
+++ webaml-backend/src/main/java/lt/vu/mif/dmsti/webaml/WebamlApplication.java
@@ -0,0 +1,13 @@
+package lt.vu.mif.dmsti.webaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebamlApplication.class, args);
+	}
+
+}
