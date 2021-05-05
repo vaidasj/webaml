2c6074da290dd74365cc6a378c72557eadfd32b8
Vaidas Jusevicius <vaidas.jusevicius@mif.vu.lt>
2021-05-05 18:49:44 +0300

    Initial commit

---
+++ webaml-backend/src/test/java/lt/vu/mif/dmsti/webaml/WebamlApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vu.mif.dmsti.webaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
