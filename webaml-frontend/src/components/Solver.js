6fa8756801a6077e3512ba9b560fb47941c48389
Vaidas Jusevicius <vaidas.jusevicius@mif.vu.lt>
2021-05-18 23:02:25 +0300

    UI to backend integration

--- webaml-frontend/src/components/Solver.js
+++ webaml-frontend/src/components/Solver.js
@@ -1,57 +1,98 @@
-import {withTheme} from '@rjsf/core';
-import {Theme as SemanticUITheme} from '@rjsf/semantic-ui';
-
-const Form = withTheme(SemanticUITheme);
-
-const schema = {
-    title: "Solve Model",
-    description: "Solve WebAML model using specific AML, solver and features",
-    type: "object",
-    required: [
-        "aml",
-        "solver"
-    ],
-    properties: {
-        aml: {
-            type: "string",
-            title: "Algebraic Modeling Language",
-            enum: ["AMPL", "GAMS", "JuMP", "Pyomo"],
-            default: "GAMS"
-        },
-        solver: {
-            type: "string",
-            title: "Solver",
-            //enum: ["GLPK", "CPLEX", "BARON", "Gurobi"],
-            //default: "GLPK"
-        }
-    }
-};
-
-const uiSchema = {
-    aml: {
-        "ui:options": {
-            "semantic" : {
-               label: ""
-            }
-        }
-
-    }
-};
-
+import Form from '@rjsf/core';
+import axios from 'axios';
+import React, { useState } from 'react';
+import {WebAMLContext} from "../App";
 
 function Solver() {
-    return (
 
-        <Form
-            schema={schema}
-            formContext={{
-                "semantic":{
-                    "wrapLabel": true,
-                    "wrapContent": true
+    const schema = {
+        title: "Solve",
+        description: "Solve WebAML model using specific AML, solver and features",
+        type: "object",
+        required: [
+            "aml",
+            "solver"
+        ],
+        properties: {
+            aml: {
+                type: "string",
+                title: "Algebraic Modeling Language",
+                enum: ["AMPL", "GAMS", "JuMP", "Pyomo"],
+                default: "GAMS"
+            },
+            solver: {
+                type: "string",
+                title: "Solver",
+                enum: ["GLPK", "CPLEX", "BARON", "Gurobi"],
+                default: "GLPK"
+            },
+            features: {
+                type: "array",
+                title: "Features",
+                uniqueItems: true,
+                items: {
+                    type: "string",
+                    enum: ["Presolve", "Parallelize"]
                 }
             }
+        }
+    };
+
+    const uiSchema = {
+        "features": {
+            "ui:widget": "checkboxes"
+        },
+    }
+
+    const context = React.useContext(WebAMLContext);
+
+    const [solution, setSolution] = useState(null);
+    const [details, setDetails] = useState(null);
+
+    const  handleCallSolver = async (aml, solver) => {
+        await axios({
+            baseURL: '/model/solve',
+            method: 'POST',
+            timeout: 1000,
+            data: context.model,
+            params: {
+                aml: aml,
+                solver: solver
             }
-        />
+        })
+        .then(response => {
+            setSolution(response.data.result);
+            setDetails(response.data.verboseOutput);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+    return (
+        <>
+            <Form
+                schema={schema}
+                uiSchema={uiSchema}
+                onSubmit={({formData}, e) => handleCallSolver(formData.aml, formData.solver)}
+            >
+                <div>
+                    <button type="submit" className="btn btn-info btn-block">Solve</button>
+                </div>
+            </Form>
+
+            <div className="panel">
+            </div>
+
+            <div className="panel panel-default" style={solution ? {} : { display: 'none' }}>
+                <div className="panel-heading"><strong>Solution</strong></div>
+                <div className="panel-body">{solution}</div>
+            </div>
+
+            <div className="panel panel-default" style={details ? {} : { display: 'none' }}>
+                <div className="panel-heading"><strong>Details</strong></div>
+                <div className="panel-body">{details}</div>
+            </div>
+        </>
     );
 }
 
